5f9942e520224928b1e3bbcdd31f2dd16c59d12e
Daniel Seetoh <danielseetoh92@gmail.com>
2017-01-01 18:28:49 +0800

    serve files through aws

--- Blogsite/settings.py
+++ Blogsite/settings.py
@@ -219,5 +219,5 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'Blogsite.custom_storages.MediaStorage'
 
